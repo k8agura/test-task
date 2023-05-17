2500c4606f2d675fc998a928e6612e91dfd2fbc5
ka8gura <rouchred@yandex.ru>
2023-05-17 23:59:45 +0400

    testing modules & connect DB

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,16 @@
 import { Controller, Get } from "@nestjs/common";
+import { AppService } from "./app.service";
 
 @Controller('/api')
 export class AppController {
+    
+    constructor (private appService: AppService) {
+
+    }
+    
     @Get('/users')
     getUsers(){
-        return[{id:1, name: 'TestTestov'}]
+        return this.appService.getUsers()
     }
 
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { SequelizeModule } from "@nestjs/sequelize";
 
 
 @Module({
-    controllers: [AppController]
+    controllers: [AppController],
+    providers: [AppService],
+    imports: [
+        SequelizeModule.forRoot({
+            dialect: 'postgres',
+            host: '127.0.0.1',
+            port: 5432,
+            username: 'postgres',
+            password: 'root',
+            database: 'test-task',
+            models: [],
+            autoLoadModels: true
+        }),
+    ]
 })
 export class AppModule {}
